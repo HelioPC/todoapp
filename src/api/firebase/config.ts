b48735802a7c94eededdde9e80880bbecf51b26a
Eliúde Vemba <sweeteliude@gmail.com>
2022-12-21 23:55:27 +0100

    Set worktree
    Set the initial worktree layout

---
+++ src/api/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_DOMAIN,
+    projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_REACT_APP_FIREBASE_APPID
+};
