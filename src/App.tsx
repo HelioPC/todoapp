b2979f22dcd95bd2815cf0911d8a630648648a2f
Eliúde Vemba <sweeteliude@gmail.com>
2022-12-25 17:41:46 +0100

    Create Home Page 🏡

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,62 @@
+import { useEffect, useState } from "react"
+import Switch from "react-switch"
+import { BsAlarm } from 'react-icons/bs'
+
 import { useAppTheme } from './contexts/Theme'
 import * as C from './style'
+import logo from './assets/images/logo.png'
+import left from './assets/images/home01.png'
+import right from './assets/images/home02.png'
+import center from './assets/images/home03.png'
 
 const App = () => {
-	const { dispatch } = useAppTheme()
+	const { dispatch, theme } = useAppTheme()
+	const [checked, setChecked] = useState(theme.name === 'light')
+
+	useEffect(() => {
+		setChecked(theme.name === 'light')
+	}, [theme])
+
 	return (
 		<C.App>
-			<h1>Olá mundo</h1>
-			<button onClick={() => dispatch.switchTheme()}>
-				Change theme
-			</button>
+			<C.AppHeader>
+				<img src={logo} alt="logo" className='h-full' />
+				
+				<ul className='flex gap-10'>
+					<li>
+						<Switch
+							height={20}
+							width={40}
+							onChange={() => dispatch.switchTheme()}
+							checked={checked}
+							checkedIcon={false}
+							uncheckedIcon={false}
+						/>
+					</li>
+					<li>
+						<a href="/">
+							Sign in
+						</a>
+					</li>
+				</ul>
+			</C.AppHeader>
+			<C.AppBody>
+				<img src={left} alt="home-img" className="h-[458px] p-16 lg:block hidden" />
+				<div className="flex flex-col items-center justify-center">
+					<div className="flex flex-col items-center gap-10">
+						<img src={logo} alt="logo" className='h-[80px] w-[100px]' />
+						<h1 className="text-center text-[42px] font-thin">To Do APP</h1>
+						<p className="text-center text-xl lg:block hidden">Seja trabalho ou lazer, o To Do facilita-lhe a vida.</p>
+						<img src={center} alt="home-img" className="lg:hidden block h-[183px]" />
+					</div>
+					<div className="pt-5 pb-[60px] flex justify-center">
+						<C.AppButton>
+							Começar já
+						</C.AppButton>
+					</div>
+				</div>
+				<img src={right} alt="home-img" className="h-[458px] p-16 lg:block hidden" />
+			</C.AppBody>
 		</C.App>
 	)
 }
--- src/style.ts
+++ src/style.ts
@@ -1,8 +1,33 @@
 import styled from "styled-components"
+import tw from "tailwind-styled-components"
 
 export const App = styled.div`
 	width: 100vw;
 	height: 100vh;
+	display: flex;
+	flex-direction: column;
 	background: ${props => props.theme.theme.colors.mainBg};
 	color: ${props => props.theme.theme.colors.textPrimary};
+
+	ul li a {
+		color: ${props => props.theme.theme.colors.textPrimary};
+	}
+`
+
+export const AppHeader = tw.header`
+	h-20 w-full py-5 sm:px-[60px] px-3 flex justify-between items-center bg-transparent
+`
+
+export const AppBody = tw.div`
+	w-full h-[calc(100vh-80px)] py-[50px] overflow-y-scroll flex flex-[1_1_auto] items-center justify-center
+`
+
+export const AppButton = styled.a`
+	background: ${props => props.theme.theme.colors.primary};
+	color: #FFF;
+	font-weight: 700;
+	padding: 14px 45px;
+	border-radius: 2px;
+	border: 0;
+	box-shadow: 0px 2px 4px -0.7px rgb(0 0 0 / 25%);
 `
